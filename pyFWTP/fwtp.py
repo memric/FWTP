1970f3496dfe35a654b5202f8c82dfbb2c0b01c9
Valeriy Chudnikov <memric@yandex.ru>
2024-07-27 20:16:36 +0300

    Change pyClient progressbar to rich progress. Format python code

--- pyFWTP/fwtp.py
+++ pyFWTP/fwtp.py
@@ -3,149 +3,151 @@ import struct
 import array
 import socket
 
-FWTP_SERVER_PORT    = 8017
-FWTP_CLIENT_PORT    = 8018
-FWTP_HDR_SIZE       = 14
-FWTP_VER            = 1
+FWTP_SERVER_PORT = 8017
+FWTP_CLIENT_PORT = 8018
+FWTP_HDR_SIZE = 14
+FWTP_VER = 1
 
 FWTP_MAX_BLOCK_SIZE = 1024
-FWTP_RX_MAX_DATA    = FWTP_MAX_BLOCK_SIZE + 16
+FWTP_RX_MAX_DATA = FWTP_MAX_BLOCK_SIZE + 16
 
-#Commands
-FWTP_CMD_NOPE       = 0 # Do nothing
-FWTP_CMD_ACK        = 1 # Acknowledge
-FWTP_CMD_RD         = 2 # Read(reserved)
-FWTP_CMD_WR         = 3 # Write block
-FWTP_CMD_START      = 4 # Transaction start
-FWTP_CMD_STOP       = 5 # Transaction stop
-FWTP_CMD_CRC        = 6 # CRC sending
-FWTP_CMD_ERR        = 7 # Error
+# Commands
+FWTP_CMD_NOPE = 0  # Do nothing
+FWTP_CMD_ACK = 1  # Acknowledge
+FWTP_CMD_RD = 2  # Read(reserved)
+FWTP_CMD_WR = 3  # Write block
+FWTP_CMD_START = 4  # Transaction start
+FWTP_CMD_STOP = 5  # Transaction stop
+FWTP_CMD_CRC = 6  # CRC sending
+FWTP_CMD_ERR = 7  # Error
 
 
 class FWTP:
-    
+
     def __init__(self, client):
         self.client = client
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(('', FWTP_CLIENT_PORT))
-        self.sock.settimeout(5) #TODO as parameter
-        
+        self.sock.bind(("", FWTP_CLIENT_PORT))
+        self.sock.settimeout(5)  # TODO as parameter
+
     # Nope command request
     def nope(self):
         packet = self.pack(FWTP_CMD_NOPE)
         resp = self.send_receive(packet)
-        
+
         if resp != None:
             payload = self.unpack(resp)
             if payload != None:
                 return True
-        
+
         return False
-    
+
     # Start command
     def start(self, file_id, ttl_size):
-        packet = self.pack(FWTP_CMD_START, file_id = file_id, ttl_size = ttl_size)
+        packet = self.pack(FWTP_CMD_START, file_id=file_id, ttl_size=ttl_size)
         resp = self.send_receive(packet)
-        
+
         if resp != None:
             payload = self.unpack(resp)
             if payload != None:
                 return True
-        
+
         return False
-        
+
     # Stop command
     def stop(self, file_id):
-        packet = self.pack(FWTP_CMD_STOP, file_id = file_id)
+        packet = self.pack(FWTP_CMD_STOP, file_id=file_id)
         resp = self.send_receive(packet)
-        
+
         if resp != None:
             payload = self.unpack(resp)
             if payload != None:
                 return True
-        
+
         return False
-        
+
     # Send block
     def write(self, file_id, ttl_size, block_offset, block_size, data):
-        packet = self.pack(FWTP_CMD_WR, file_id, ttl_size, block_offset, block_size, data)
+        packet = self.pack(
+            FWTP_CMD_WR, file_id, ttl_size, block_offset, block_size, data
+        )
         resp = self.send_receive(packet)
-        
+
         if resp != None:
             payload = self.unpack(resp)
             if payload != None:
                 return True
-        
+
         return False
-      
+
     # Packet composer
-    def pack(self, cmd, file_id = 0, ttl_size = 0, block_offset = 0, block_size = 0, data = None):
-        #Construct header
-        packet = struct.pack('<BBHLLH',
-            FWTP_VER << 6 | cmd << 3,               # Set version and command
-            file_id,                                # File ID
-            1,                                      # Packet ID
-            ttl_size,                               # Total file size
-            block_offset,                           # Block offset
-            block_size                              # Block size
+    def pack(self, cmd, file_id=0, ttl_size=0, block_offset=0, block_size=0, data=None):
+        # Construct header
+        packet = struct.pack(
+            "<BBHLLH",
+            FWTP_VER << 6 | cmd << 3,  # Set version and command
+            file_id,  # File ID
+            1,  # Packet ID
+            ttl_size,  # Total file size
+            block_offset,  # Block offset
+            block_size,  # Block size
         )
-        
-        #add data
+
+        # add data
         if data != None:
             packet += data
-        
-        #CRC calculation
+
+        # CRC calculation
         crc = self.getCRC(packet)
-        packet += struct.pack('<H', crc)
-        
+        packet += struct.pack("<H", crc)
+
         return packet
-        
+
     # Packet parser
     def unpack(self, data):
-        #check packet size
+        # check packet size
         if len(data) < (FWTP_HDR_SIZE + 2):
-            print("Incorrect packet size") #TODO remove
+            print("Incorrect packet size")  # TODO remove
             return None
-        
-        #check packet CRC
-        recv_crc = struct.unpack('<H', data[-2:])
+
+        # check packet CRC
+        recv_crc = struct.unpack("<H", data[-2:])
         calc_crc = self.getCRC(data[:-2])
         if recv_crc != calc_crc:
-            print("Incorrect CRC") #TODO remove
+            print("Incorrect CRC")  # TODO remove
             return None
-        
-        #get header values
+
+        # get header values
         header = data[:FWTP_HDR_SIZE]
-        hdr_params = struct.unpack('<BBHLLH', header)
-#        print(hdr_params)
-        
-        #get version
+        hdr_params = struct.unpack("<BBHLLH", header)
+        #        print(hdr_params)
+
+        # get version
         if (hdr_params[0] >> 6) != FWTP_VER:
-            print("Incorrect version") #TODO remove
+            print("Incorrect version")  # TODO remove
             return None
-        
-        #get command
+
+        # get command
         if ((hdr_params[0] >> 3) & 0x7) != FWTP_CMD_ACK:
-            print("Is not acknowledge command") #TODO remove
+            print("Is not acknowledge command")  # TODO remove
             return None
-        
-        #return payload
+
+        # return payload
         return data[FWTP_HDR_SIZE:]
-        
+
     # Sends packet to server and receives response
     def send_receive(self, packet):
         self.sock.sendto(packet, (self.client, FWTP_SERVER_PORT))
         try:
             data, src = self.sock.recvfrom(FWTP_RX_MAX_DATA)
-            #TODO Check src
+            # TODO Check src
             return data
         except:
             return None
-        
 
     def getCRC(self, data):
         crc = np.uint16(0xFFFF)
-        pdata = array.array('B', data)
+        pdata = array.array("B", data)
         size = len(pdata)
         for i in range(size):
             crc ^= np.uint16(pdata[i] << 8)
@@ -155,4 +157,3 @@ class FWTP:
                 else:
                     crc = np.uint16(crc << 1)
         return np.uint16(crc & 0xFFFF)
-
--- pyFWTP/fwtp_client.py
+++ pyFWTP/fwtp_client.py
@@ -3,98 +3,115 @@
 import sys
 from argparse import ArgumentParser
 from fwtp import FWTP
-from tqdm import tqdm
+from rich.progress import track
 from time import sleep
+
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
     else:
         sys.argv.extend(argv)
-    
+
     try:
         # Setup argument parser
-        parser = ArgumentParser(description = 'FWTP client.')
-        parser.add_argument('file', help='File to send', metavar='file')
-        parser.add_argument('-a', '--addr', dest='ip', default='127.0.0.1', help='Server IP address')
-        parser.add_argument('-i', '--id', dest='id', default=0x10, help='File ID')
-        parser.add_argument('-s', '--size', dest='block_size', default=256, help='Block size')
-#        parser.add_argument('-p', '--pause', dest='pause', default=100, help='Cycle pause')
-        parser.add_argument('--version', action='version', version='0.0.1')
-        
+        parser = ArgumentParser(description="FWTP client.")
+        parser.add_argument("file", help="File to send", metavar="file")
+        parser.add_argument(
+            "-a", "--addr", dest="ip", default="127.0.0.1", help="Server IP address"
+        )
+        parser.add_argument("-i", "--id", dest="id", default=0x10, help="File ID")
+        parser.add_argument(
+            "-s", "--size", dest="block_size", default=256, help="Block size"
+        )
+        #        parser.add_argument('-p', '--pause', dest='pause', default=100, help='Cycle pause')
+        parser.add_argument("--version", action="version", version="0.0.1")
+
         # Process arguments
         args = parser.parse_args()
-        
+
         file_name = args.file
         file_id = int(args.id)
         ip = args.ip
         block_size = int(args.block_size)
-        
+
         try:
-            file = open(file_name, 'rb')
+            file = open(file_name, "rb")
             file_data = file.read()
             file_len = len(file_data)
             blocks_num = int((file_len + block_size - 1) / block_size)
-            
-            print('File size: %d; Blocks: %d; Block size %d'%(file_len, blocks_num, block_size))
+
+            print(
+                "File size: %d; Blocks: %d; Block size %d"
+                % (file_len, blocks_num, block_size)
+            )
         except:
             print("Can't open file")
             return 1
-        
+
         client = FWTP(ip)
-        
+
         # Nope commant to check link
-        print('Check server on IP: %s -> '%(ip), end='')
+        print("Check server on IP: %s -> " % (ip), end="")
         if client.nope() == True:
-            print('PASSED')
+            print("PASSED")
         else:
-            print('FAILED')
-            return 1
-            
-        # Start command
-        print('Start file transfer -> ', end='')
-        if client.start(file_id, file_len) == True:
-            print('OK')
-        else:
-            print('FAILED')
+            print("FAILED")
             return 1
 
-        for i in tqdm(range(blocks_num)):
-            #send block
-            offset = block_size*i
+        # Start command
+        print("Start file transfer -> ", end="")
+        if client.start(file_id, file_len) == True:
+            print("OK")
+        else:
+            print("FAILED")
+            return 1
+
+        for i in track(range(blocks_num), description="Sending..."):
+            # send block
+            offset = block_size * i
             curr_block_size = block_size
             if (file_len - offset) < block_size:
                 curr_block_size = file_len - offset
             else:
                 curr_block_size = block_size
-                
-#            tqdm.write('Block %d; Offset %d; Size %d'%(i, offset, curr_block_size))
-            
-            if client.write(file_id, file_len, offset, curr_block_size, file_data[offset:offset+curr_block_size]) == False:
-                tqdm.write('Block %d sending error'%(i))
+
+            #            tqdm.write('Block %d; Offset %d; Size %d'%(i, offset, curr_block_size))
+
+            if (
+                client.write(
+                    file_id,
+                    file_len,
+                    offset,
+                    curr_block_size,
+                    file_data[offset : offset + curr_block_size],
+                )
+                == False
+            ):
+                print("Block %d sending error" % (i))
                 return 1
-                
-#            sleep(0.1)
+
+        #            sleep(0.1)
 
         # Stop
-        print('Stop file transfer -> ', end='')
+        print("Stop file transfer -> ", end="")
         if client.stop(file_id) == True:
-            print('OK')
+            print("OK")
         else:
-            print('FAILED')
+            print("FAILED")
             return 1
 
         file.close()
-            
+
         return 0
-                
+
     except KeyboardInterrupt:
         ### handle keyboard interrupt ###
         return 0
     except Exception as e:
         sys.stderr.write(" for help use --help")
         return 2
-        
+
+
 if __name__ == "__main__":
     sys.exit(main())
-
