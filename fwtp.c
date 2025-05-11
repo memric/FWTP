f581c53cf734a51b5872f710b4a95f20f24536f2
Valeriy Chudnikov <memric@yandex.ru>
2025-05-11 15:23:10 +0300

    Format

--- fwtp.c
+++ fwtp.c
@@ -11,44 +11,44 @@
 #include <string.h>
 #if defined(__linux__) || (__APPLE__)
 #include <netinet/in.h>
+#include <pthread.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <unistd.h>
 #include <time.h>
-#include <pthread.h>
+#include <unistd.h>
 #elif defined(ESP_PLATFORM)
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "lwip/opt.h"
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
-#include "esp_log.h"
-#elif _RTOS_ //This means LwIP usage
+#elif _RTOS_ // This means LwIP usage
 #include "FreeRTOS.h"
-#include "task.h"
 #include "lwip/opt.h"
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
+#include "task.h"
 #else
 #error "OS is not supported!"
 #endif
 
 #if defined(ESP_PLATFORM) || _RTOS_
-#define FWTP_THREAD_STACK	(4*1024)
-#define FWTP_THREAD_PRIO	(tskIDLE_PRIORITY + 2)
+#define FWTP_THREAD_STACK (4 * 1024)
+#define FWTP_THREAD_PRIO  (tskIDLE_PRIORITY + 2)
 #endif
 
 #if defined(ESP_PLATFORM)
-#define PTRACE(m...)		ESP_LOGI("FWTP", m)
-#define PTRACE_ERR(m...)	ESP_LOGE("FWTP", m)
-#define PTRACE_WRN(m...)	ESP_LOGW("FWTP", m)
+#define PTRACE(m...)     ESP_LOGI("FWTP", m)
+#define PTRACE_ERR(m...) ESP_LOGE("FWTP", m)
+#define PTRACE_WRN(m...) ESP_LOGW("FWTP", m)
 #endif
 
 #ifndef PTRACE
 #include <stdio.h>
-#define PTRACE				printf
-#define PTRACE_ERR			printf
-#define PTRACE_WRN			printf
+#define PTRACE     printf
+#define PTRACE_ERR printf
+#define PTRACE_WRN printf
 #endif
 
 #if defined(__linux__) || (__APPLE__)
@@ -61,12 +61,12 @@ static uint8_t FWTP_RX_Buffer[FWTP_RX_MAX_DATA];
 static uint8_t FWTP_TX_Buffer[FWTP_TX_MAX_DATA];
 
 #if defined(__linux__) || (__APPLE__)
-void* FWTP_ServerThread(void * argument);
+void *FWTP_ServerThread(void *argument);
 #else
-void FWTP_ServerThread(void * argument);
+void FWTP_ServerThread(void *argument);
 #endif
-uint32_t FWTP_PacketParser(uint8_t *p, uint16_t len);
-uint32_t FWTP_AckSend(struct fwtp_hdr* hdr, int sock, struct sockaddr_in* src);
+uint32_t FWTP_PacketParser(const uint8_t *p, uint16_t len);
+uint32_t FWTP_AckSend(struct fwtp_hdr *hdr, int sock, struct sockaddr_in *src);
 
 /**
  * @brief   FWTP Initialization
@@ -74,16 +74,18 @@ uint32_t FWTP_AckSend(struct fwtp_hdr* hdr, int sock, struct sockaddr_in* src);
  */
 uint32_t FWTP_Init(void)
 {
-	if (hFWTP != NULL) return 1;
+    if (hFWTP != NULL)
+        return 1;
 
 #if defined(__linux__) || (__APPLE__)
-	pthread_create(&hFWTP, NULL, FWTPServerThread, NULL);
-	if (pthread_join(hFWTP, NULL)) return 1;
+    pthread_create(&hFWTP, NULL, FWTPServerThread, NULL);
+    if (pthread_join(hFWTP, NULL))
+        return 1;
 #elif defined(ESP_PLATFORM) || _RTOS_
-	xTaskCreate(FWTP_ServerThread, "FWTP Thread", FWTP_THREAD_STACK, NULL, FWTP_THREAD_PRIO, &hFWTP);
+    xTaskCreate(FWTP_ServerThread, "FWTP Thread", FWTP_THREAD_STACK, NULL, FWTP_THREAD_PRIO, &hFWTP);
 #endif
 
-	return FWTP_ERR_OK;
+    return FWTP_ERR_OK;
 }
 
 /**
@@ -91,76 +93,77 @@ uint32_t FWTP_Init(void)
  * @param   argument
  */
 #if defined(__linux__) || (__APPLE__)
-void* FWTP_ServerThread(void * argument)
+void *FWTP_ServerThread(void *argument)
 #else
-void FWTP_ServerThread(void * argument)
+void FWTP_ServerThread(void *argument)
 #endif
 {
-	int32_t err, recv_len;
-	struct sockaddr_in source_addr;
-	socklen_t addrLen;
+    int32_t err, recv_len;
+    struct sockaddr_in source_addr;
+    socklen_t addrLen;
 
-	(void) argument;
+    (void) argument;
 
-	PTRACE("Creating a new FWTP server\r\n");
+    PTRACE("Creating a new FWTP server\r\n");
 
-	/*Create new socket*/
-	int sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    /*Create new socket*/
+    int sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-	if (sock != -1)
-	{
-		struct sockaddr_in addr;
-		/* set up address to connect to */
-		memset(&addr, 0, sizeof(addr));
-		addr.sin_family = AF_INET;
-		addr.sin_port = htons(FWTP_SERVER_PORT);
-		addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    if (sock != -1)
+    {
+        struct sockaddr_in addr;
+        /* set up address to connect to */
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_family      = AF_INET;
+        addr.sin_port        = htons(FWTP_SERVER_PORT);
+        addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-		/* Bind connection to port UDP_PORT. */
-		err = bind(sock, (struct sockaddr *) &addr, sizeof(addr));
+        /* Bind connection to port UDP_PORT. */
+        err                  = bind(sock, (struct sockaddr *) &addr, sizeof(addr));
 
-		if (err == 0)
-		{
-			while (1)
-			{
-				addrLen = sizeof(struct sockaddr);
-				recv_len = recvfrom(sock, FWTP_RX_Buffer, sizeof(FWTP_RX_Buffer), 0, ( struct sockaddr* ) &source_addr, &addrLen);
+        if (err == 0)
+        {
+            while (1)
+            {
+                addrLen  = sizeof(struct sockaddr);
+                recv_len = recvfrom(sock, FWTP_RX_Buffer, sizeof(FWTP_RX_Buffer), 0, (struct sockaddr *) &source_addr,
+                                    &addrLen);
 
-				/*If data is present*/
-				if (recv_len > 0)
-				{
-					if (FWTP_PacketParser(FWTP_RX_Buffer, recv_len) == FWTP_ERR_OK)
-					{
-						FWTP_AckSend((struct fwtp_hdr*) FWTP_RX_Buffer, sock, &source_addr);
+                /*If data is present*/
+                if (recv_len > 0)
+                {
+                    if (FWTP_PacketParser(FWTP_RX_Buffer, recv_len) == FWTP_ERR_OK)
+                    {
+                        FWTP_AckSend((struct fwtp_hdr *) FWTP_RX_Buffer, sock, &source_addr);
 
-						/*To do after acknowledge*/
-						FWTP_PendingOperation((struct fwtp_hdr*) FWTP_RX_Buffer);
-					}
-					else
-					{
-						PTRACE_ERR("FWTP error\r\n");
+                        /*To do after acknowledge*/
+                        FWTP_PendingOperation((struct fwtp_hdr *) FWTP_RX_Buffer);
+                    }
+                    else
+                    {
+                        PTRACE_ERR("FWTP error\r\n");
 
-						//TODO Is Error need to be sent?
-					}
-				}
-			}
+                        // TODO Is Error need to be sent?
+                    }
+                }
+            }
 
-			close(sock);
-		}
-		else
-		{
-			PTRACE_ERR("Can't bind FWTP server to port %d\r\n", FWTP_SERVER_PORT);
-		}
-	}
-	else
-	{
-		PTRACE_ERR("Can't to create FWTP server\r\n");
-	}
+            close(sock);
+        }
+        else
+        {
+            PTRACE_ERR("Can't bind FWTP server to port %d\r\n", FWTP_SERVER_PORT);
+        }
+    }
+    else
+    {
+        PTRACE_ERR("Can't to create FWTP server\r\n");
+    }
 
 #if defined(__linux__) || (__APPLE__)
-	return NULL;
+    return NULL;
 #elif defined(ESP_PLATFORM) || _RTOS_
-	vTaskDelete(NULL);
+    vTaskDelete(NULL);
 #endif
 }
 
@@ -169,72 +172,72 @@ void FWTP_ServerThread(void * argument)
  * @param p     Pointer to data
  * @param len   Data length
  */
-uint32_t FWTP_PacketParser(uint8_t *p, uint16_t len)
+uint32_t FWTP_PacketParser(const uint8_t *p, uint16_t len)
 {
-	if (len < sizeof(struct fwtp_hdr) + 2)
-	{
+    if (len < sizeof(struct fwtp_hdr) + 2)
+    {
         PTRACE_ERR("Invalid packet length\r\n");
-		return FWTP_ERR_SIZE;
-	}
+        return FWTP_ERR_SIZE;
+    }
 
-	/*Calculate CRC*/
-	uint16_t block_crc16 = *((uint16_t *) &p[len-2]);
-	uint16_t calc_crc16 = FWTP_CRC(p, len-2);
-	if (block_crc16 != calc_crc16)
-	{
-		PTRACE_ERR("Invalid CRC: expected %d, obtained %d\r\n", calc_crc16, block_crc16);
-		return FWTP_ERR_CRC;
-	}
+    /*Calculate CRC*/
+    uint16_t block_crc16 = *((uint16_t *) &p[len - 2]);
+    uint16_t calc_crc16  = FWTP_CRC(p, len - 2);
+    if (block_crc16 != calc_crc16)
+    {
+        PTRACE_ERR("Invalid CRC: expected %d, obtained %d\r\n", calc_crc16, block_crc16);
+        return FWTP_ERR_CRC;
+    }
 
-	struct fwtp_hdr* hdr = (struct fwtp_hdr*) p;
+    struct fwtp_hdr *hdr = (struct fwtp_hdr *) p;
 
-	if (FWTP_HDR_GET_VER(hdr) != FWTP_VER)
-	{
-		PTRACE_ERR("FWTP unsupported version: supported %d, obtained %d\r\n", FWTP_VER, FWTP_HDR_GET_VER(hdr));
-		return FWTP_ERR_VER;
-	}
-    
-	/*Check command*/
+    if (FWTP_HDR_GET_VER(hdr) != FWTP_VER)
+    {
+        PTRACE_ERR("FWTP unsupported version: supported %d, obtained %d\r\n", FWTP_VER, FWTP_HDR_GET_VER(hdr));
+        return FWTP_ERR_VER;
+    }
+
+    /*Check command*/
     if (FWTP_HDR_GET_CMD(hdr) == FWTP_CMD_NOPE)
     {
         PTRACE("Nope command received\r\n");
         return FWTP_ERR_OK;
     }
     else if (FWTP_HDR_GET_CMD(hdr) == FWTP_CMD_START)
-	{
-		return FWTP_FileStart(hdr->file_id, hdr->file_size);
-	}
+    {
+        return FWTP_FileStart(hdr->file_id, hdr->file_size);
+    }
     else if (FWTP_HDR_GET_CMD(hdr) == FWTP_CMD_STOP)
-	{
-		return FWTP_FileStop(hdr->file_id);
-	}
+    {
+        return FWTP_FileStop(hdr->file_id);
+    }
     else if (FWTP_HDR_GET_CMD(hdr) == FWTP_CMD_WR)
-	{
-		/*Check block size*/
-		if (hdr->block_size != (len - 2 - sizeof(struct fwtp_hdr)))
-		{
-			PTRACE_ERR("Incorrect block size\r\n");
-			return FWTP_ERR_SIZE;
-		}
+    {
+        /*Check block size*/
+        if (hdr->block_size != (len - 2 - sizeof(struct fwtp_hdr)))
+        {
+            PTRACE_ERR("Incorrect block size\r\n");
+            return FWTP_ERR_SIZE;
+        }
 
-		/*Write data block*/
-		if (hdr->block_size == FWTP_BlockWrite(hdr->file_id, hdr->file_size, hdr->block_offset,
-				hdr->block_size, &p[sizeof(struct fwtp_hdr)]))
-		{
-			/*OK*/
-		}
-		else
-		{
-			PTRACE_ERR("Block writing error\r\n");
-			return FWTP_ERR_FS;
-		}
-	}
-	else
-	{
-		return FWTP_ERR_CMD;
-	}
+        /*Write data block*/
+        if (hdr->block_size == FWTP_BlockWrite(hdr->file_id, hdr->file_size, hdr->block_offset, hdr->block_size,
+                                               &p[sizeof(struct fwtp_hdr)]))
+        {
+            /*OK*/
+        }
+        else
+        {
+            PTRACE_ERR("Block writing error\r\n");
+            return FWTP_ERR_FS;
+        }
+    }
+    else
+    {
+        return FWTP_ERR_CMD;
+    }
 
-	return FWTP_ERR_OK;
+    return FWTP_ERR_OK;
 }
 
 /**
@@ -247,18 +250,19 @@ uint32_t FWTP_PacketParser(uint8_t *p, uint16_t len)
  * @param p         Pointer to block data
  * @return          Number of written bytes
  */
-__attribute__((weak))
-uint32_t FWTP_BlockWrite(uint8_t file_id, uint32_t ttl_fsize, uint32_t offset, uint16_t len, uint8_t *p)
+__attribute__((weak)) uint32_t FWTP_BlockWrite(uint8_t file_id, uint32_t ttl_fsize, uint32_t offset, uint16_t len,
+                                               const uint8_t *p)
 {
-	if (file_id != FWTP_MAINSYSTEM_FILE_ID) return 0;
+    if (file_id != FWTP_MAINSYSTEM_FILE_ID)
+        return 0;
 
-	/* TODO write routine*/
-	PTRACE("Block received; File ID: %u; Offset: %lu; Size: %hu;\r\n", file_id, offset, len);
+    /* TODO write routine*/
+    PTRACE("Block received; File ID: %u; Offset: %lu; Size: %hu;\r\n", file_id, offset, len);
 
-	(void) ttl_fsize;
-	(void) p;
+    (void) ttl_fsize;
+    (void) p;
 
-	return len;
+    return len;
 }
 
 /**
@@ -268,14 +272,14 @@ uint32_t FWTP_BlockWrite(uint8_t file_id, uint32_t ttl_fsize, uint32_t offset, u
  * @param ttl_fsize Total file size
  * @return          Error code. FWTP_ERR_OK for success.
  */
-__attribute__((weak))
-uint32_t FWTP_FileStart(uint8_t file_id, uint32_t ttl_fsize)
+__attribute__((weak)) uint32_t FWTP_FileStart(uint8_t file_id, uint32_t ttl_fsize)
 {
-	if (file_id != FWTP_MAINSYSTEM_FILE_ID) return FWTP_ERR_SUBSYSTEM;
+    if (file_id != FWTP_MAINSYSTEM_FILE_ID)
+        return FWTP_ERR_SUBSYSTEM;
 
-	PTRACE("File start command received; File ID: %u; Size: %lu\r\n", file_id, ttl_fsize);
+    PTRACE("File start command received; File ID: %u; Size: %lu\r\n", file_id, ttl_fsize);
 
-	return FWTP_ERR_OK;
+    return FWTP_ERR_OK;
 }
 
 /**
@@ -284,14 +288,14 @@ uint32_t FWTP_FileStart(uint8_t file_id, uint32_t ttl_fsize)
  * @param file_id   File ID
  * @return          Error code. FWTP_ERR_OK for success.
  */
-__attribute__((weak))
-uint32_t FWTP_FileStop(uint8_t file_id)
+__attribute__((weak)) uint32_t FWTP_FileStop(uint8_t file_id)
 {
-	if (file_id != FWTP_MAINSYSTEM_FILE_ID) return FWTP_ERR_SUBSYSTEM;
+    if (file_id != FWTP_MAINSYSTEM_FILE_ID)
+        return FWTP_ERR_SUBSYSTEM;
 
-	PTRACE("File stop command received; File ID: %u\r\n", file_id);
+    PTRACE("File stop command received; File ID: %u\r\n", file_id);
 
-	return FWTP_ERR_OK;
+    return FWTP_ERR_OK;
 }
 
 /**
@@ -300,45 +304,44 @@ uint32_t FWTP_FileStop(uint8_t file_id)
  * @param sock  Socket
  * @param src   Client address
  */
-uint32_t FWTP_AckSend(struct fwtp_hdr* hdr, int sock, struct sockaddr_in* src)
+uint32_t FWTP_AckSend(struct fwtp_hdr *hdr, int sock, struct sockaddr_in *src)
 {
-	struct sockaddr_in addr;
+    struct sockaddr_in addr;
 
-	/* set up address to connect to */
-	memset(&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(FWTP_CLIENT_PORT);
-	addr.sin_addr.s_addr = src->sin_addr.s_addr;
+    /* set up address to connect to */
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family         = AF_INET;
+    addr.sin_port           = htons(FWTP_CLIENT_PORT);
+    addr.sin_addr.s_addr    = src->sin_addr.s_addr;
 
-	struct fwtp_hdr* tx_hdr = (struct fwtp_hdr*) FWTP_TX_Buffer;
+    struct fwtp_hdr *tx_hdr = (struct fwtp_hdr *) FWTP_TX_Buffer;
 
-	memcpy(tx_hdr, hdr, sizeof(struct fwtp_hdr));
-	tx_hdr->hdr = 0;
-	FWTP_HDR_SET_VER(tx_hdr, FWTP_VER);
-	FWTP_HDR_SET_CMD(tx_hdr, FWTP_CMD_ACK);
-	FWTP_HDR_SET_ATTR(tx_hdr, FWTP_ATTR_NONE);
+    memcpy(tx_hdr, hdr, sizeof(struct fwtp_hdr));
+    tx_hdr->hdr = 0;
+    FWTP_HDR_SET_VER(tx_hdr, FWTP_VER);
+    FWTP_HDR_SET_CMD(tx_hdr, FWTP_CMD_ACK);
+    FWTP_HDR_SET_ATTR(tx_hdr, FWTP_ATTR_NONE);
 
-	/*Calculate CRC*/
-	uint16_t *pblock_crc16 = (uint16_t *) &FWTP_TX_Buffer[sizeof(struct fwtp_hdr)];
-	*pblock_crc16 = FWTP_CRC(FWTP_TX_Buffer, sizeof(struct fwtp_hdr));
+    /*Calculate CRC*/
+    uint16_t *pblock_crc16 = (uint16_t *) &FWTP_TX_Buffer[sizeof(struct fwtp_hdr)];
+    *pblock_crc16          = FWTP_CRC(FWTP_TX_Buffer, sizeof(struct fwtp_hdr));
 
-	/* Send to client*/
-	if (sendto(sock, FWTP_TX_Buffer, sizeof(struct fwtp_hdr) + 2, 0,
-			( struct sockaddr* ) &addr, sizeof(struct sockaddr_in) ) <= 0)
-	{
-		PTRACE_ERR("Acknowledge sending error: socket %i\r\n", sock);
-		return FWTP_ERR_NET;
-	}
+    /* Send to client*/
+    if (sendto(sock, FWTP_TX_Buffer, sizeof(struct fwtp_hdr) + 2, 0, (struct sockaddr *) &addr,
+               sizeof(struct sockaddr_in)) <= 0)
+    {
+        PTRACE_ERR("Acknowledge sending error: socket %i\r\n", sock);
+        return FWTP_ERR_NET;
+    }
 
-	return FWTP_ERR_OK;
+    return FWTP_ERR_OK;
 }
 
 /**
  * @brief       Invoked after acknowledge sending
  * @param hdr   Pointer to received packet header
  */
-__attribute__((weak))
-void FWTP_PendingOperation(struct fwtp_hdr* hdr)
+__attribute__((weak)) void FWTP_PendingOperation(struct fwtp_hdr *hdr)
 {
     (void) hdr;
 }
@@ -349,17 +352,18 @@ void FWTP_PendingOperation(struct fwtp_hdr* hdr)
  * @param len   Data size
  * @return      CRC value
  */
-uint16_t FWTP_CRC(uint8_t * pdata, uint16_t len)
+uint16_t FWTP_CRC(const uint8_t *pdata, uint16_t len)
 {
-	uint16_t crc = 0xFFFF;
-	uint8_t i;
+    uint16_t crc = 0xFFFF;
+    uint8_t i;
 
-	while (len--)
-	{
-		crc ^= *pdata++ << 8;
+    while (len--)
+    {
+        crc ^= *pdata++ << 8;
 
-		for (i = 0; i < 8; i++)
-			crc = crc & 0x8000 ? (crc << 1) ^ 0x1021 : crc << 1;
-	}
-	return crc;
+        for (i = 0; i < 8; i++)
+            crc = crc & 0x8000 ? (crc << 1) ^ 0x1021 : crc << 1;
+    }
+
+    return crc;
 }
--- fwtp.h
+++ fwtp.h
@@ -11,91 +11,92 @@
 #include <stdint.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-#define FWTP_VER					1 /*Supported version*/
+#define FWTP_VER         1 /*Supported version*/
 
-#define FWTP_SERVER_PORT			8017
-#define FWTP_CLIENT_PORT            8018
+#define FWTP_SERVER_PORT 8017
+#define FWTP_CLIENT_PORT 8018
 #ifndef FWTP_MAX_BLOCK_SIZE
-#define FWTP_MAX_BLOCK_SIZE			1024
+#define FWTP_MAX_BLOCK_SIZE 1024
 #endif
-#define FWTP_RX_MAX_DATA			(FWTP_MAX_BLOCK_SIZE + 16)
-#define FWTP_TX_MAX_DATA			64
+#define FWTP_RX_MAX_DATA        (FWTP_MAX_BLOCK_SIZE + 16)
+#define FWTP_TX_MAX_DATA        64
 
-#define FWTP_BOOTLOADER_FILE_ID		0x01
-#define FWTP_MAINSYSTEM_FILE_ID		0x10
-#define FWTP_SUBSYSTEM1_FILE_ID		0x21
-#define FWTP_SUBSYSTEM2_FILE_ID		0x22
-#define FWTP_SUBSYSTEM3_FILE_ID		0x23
+#define FWTP_BOOTLOADER_FILE_ID 0x01
+#define FWTP_MAINSYSTEM_FILE_ID 0x10
+#define FWTP_SUBSYSTEM1_FILE_ID 0x21
+#define FWTP_SUBSYSTEM2_FILE_ID 0x22
+#define FWTP_SUBSYSTEM3_FILE_ID 0x23
 
-#define FWTP_CMD_NOPE				0 /*Nope*/
-#define FWTP_CMD_ACK				1 /*Acknowledge*/
-#define FWTP_CMD_RD					2 /*Read command (reserved)*/
-#define FWTP_CMD_WR					3 /*Write command*/
-#define FWTP_CMD_START				4 /*Transaction start*/
-#define FWTP_CMD_STOP				5 /*Transaction stop*/
-#define FWTP_CMD_CRC				6 /*CRC sending*/
-#define FWTP_CMD_ERR				7 /*Error*/
+#define FWTP_CMD_NOPE           0 /*Nope*/
+#define FWTP_CMD_ACK            1 /*Acknowledge*/
+#define FWTP_CMD_RD             2 /*Read command (reserved)*/
+#define FWTP_CMD_WR             3 /*Write command*/
+#define FWTP_CMD_START          4 /*Transaction start*/
+#define FWTP_CMD_STOP           5 /*Transaction stop*/
+#define FWTP_CMD_CRC            6 /*CRC sending*/
+#define FWTP_CMD_ERR            7 /*Error*/
 
-#define FWTP_ATTR_NONE              0 /*No attributes*/
+#define FWTP_ATTR_NONE          0 /*No attributes*/
 
-#define FWTP_HDR_GET_VER(a)			((a)->hdr >> 6) /*Get version from header*/
-#define FWTP_HDR_GET_CMD(a)			(((a)->hdr >> 3) & 0x7) /*Get command from header*/
-#define FWTP_HDR_GET_ATTR(a)		((a)->hdr & 0x7) /*Get attributes from header*/
-#define FWTP_HDR_SET_VER(a, c)		((a)->hdr |= (c) << 6)
-#define FWTP_HDR_SET_CMD(a, c)		((a)->hdr |= (c) << 3)
-#define FWTP_HDR_SET_ATTR(a, c)		((a)->hdr |= (c))
+#define FWTP_HDR_GET_VER(a)     ((a)->hdr >> 6)         /*Get version from header*/
+#define FWTP_HDR_GET_CMD(a)     (((a)->hdr >> 3) & 0x7) /*Get command from header*/
+#define FWTP_HDR_GET_ATTR(a)    ((a)->hdr & 0x7)        /*Get attributes from header*/
+#define FWTP_HDR_SET_VER(a, c)  ((a)->hdr |= (c) << 6)
+#define FWTP_HDR_SET_CMD(a, c)  ((a)->hdr |= (c) << 3)
+#define FWTP_HDR_SET_ATTR(a, c) ((a)->hdr |= (c))
 
-#define FWTP_HDR_SIZE				sizeof(struct fwtp_hdr)
+#define FWTP_HDR_SIZE           sizeof(struct fwtp_hdr)
 
-#define FWTP_ERR_OK					0x00
-#define FWTP_ERR_SIZE				0x01
-#define FWTP_ERR_CRC				0x02
-#define FWTP_ERR_VER				0x03
-#define FWTP_ERR_CMD				0x04
-#define FWTP_ERR_FILE_ID			0x05
-#define FWTP_ERR_FS					0x10
-#define FWTP_ERR_NET				0x11
-#define FWTP_ERR_SUBSYSTEM			0x20
+#define FWTP_ERR_OK             0x00
+#define FWTP_ERR_SIZE           0x01
+#define FWTP_ERR_CRC            0x02
+#define FWTP_ERR_VER            0x03
+#define FWTP_ERR_CMD            0x04
+#define FWTP_ERR_FILE_ID        0x05
+#define FWTP_ERR_FS             0x10
+#define FWTP_ERR_NET            0x11
+#define FWTP_ERR_SUBSYSTEM      0x20
 
+    /**
+     * Packet Structure
+     * -----------------------
+     * 1 byte:	Version (2 bits) + Command (3 bits) + Attributes (3 bits reserved)
+     * -----------------------
+     * 1 byte:  File ID
+     * -----------------------
+     * 2 bytes:	Packet ID (reserved)
+     * -----------------------
+     * 4 bytes: Total file size
+     * -----------------------
+     * 4 bytes: Block offset
+     * -----------------------
+     * 2 bytes: Block size
+     * -----------------------
+     * N bytes: Data...
+     * -----------------------
+     * 2 bytes: Block CRC16
+     */
 
-/**
- * Packet Structure
- * -----------------------
- * 1 byte:	Version (2 bits) + Command (3 bits) + Attributes (3 bits reserved)
- * -----------------------
- * 1 byte:  File ID
- * -----------------------
- * 2 bytes:	Packet ID (reserved)
- * -----------------------
- * 4 bytes: Total file size
- * -----------------------
- * 4 bytes: Block offset
- * -----------------------
- * 2 bytes: Block size
- * -----------------------
- * N bytes: Data...
- * -----------------------
- * 2 bytes: Block CRC16
- */
+    struct fwtp_hdr
+    {
+        uint8_t hdr;
+        uint8_t file_id;
+        uint16_t packet_id;
+        uint32_t file_size;
+        uint32_t block_offset;
+        uint16_t block_size;
+    } __attribute__((__packed__));
 
-struct fwtp_hdr {
-	uint8_t hdr;
-	uint8_t file_id;
-	uint16_t packet_id;
-	uint32_t file_size;
-	uint32_t block_offset;
-	uint16_t block_size;
-} __attribute__ ((__packed__));
-
-uint32_t FWTP_Init(void);
-uint16_t FWTP_CRC(uint8_t * pdata, uint16_t len);
-uint32_t FWTP_BlockWrite(uint8_t file_id, uint32_t ttl_fsize, uint32_t offset, uint16_t len, uint8_t *p);
-uint32_t FWTP_FileStart(uint8_t file_id, uint32_t ttl_fsize);
-uint32_t FWTP_FileStop(uint8_t file_id);
-void FWTP_PendingOperation(struct fwtp_hdr* hdr);
+    uint32_t FWTP_Init(void);
+    uint16_t FWTP_CRC(const uint8_t *pdata, uint16_t len);
+    uint32_t FWTP_BlockWrite(uint8_t file_id, uint32_t ttl_fsize, uint32_t offset, uint16_t len, const uint8_t *p);
+    uint32_t FWTP_FileStart(uint8_t file_id, uint32_t ttl_fsize);
+    uint32_t FWTP_FileStop(uint8_t file_id);
+    void FWTP_PendingOperation(struct fwtp_hdr *hdr);
 
 #ifdef __cplusplus
 }
